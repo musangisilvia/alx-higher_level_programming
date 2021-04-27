feae2c88ec17e8dadad79ac6ff699700bbff7025
musangisilvia <musangisilvia@gmail.com>
2021-04-27 19:44:20 +0000

    add a condition if < 6 and != 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,11 +9,11 @@ else:
     lastDigit = number % 10
 
 # check the last digit of the number
-if lastDigit > 5:
+if lastDigit == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, lastDigit))
+elif lastDigit > 5:
     print("Last digit of {:d} is {:d} and is greater\
  than 5".format(number, lastDigit))
-elif lastDigit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, lastDigit))
-else:
+elif lastDigit < 6 and lastDigit != 0:
     print("Last digit of {:d} is {:d} and is\
  less than 6 and not 0".format(number, lastDigit))
