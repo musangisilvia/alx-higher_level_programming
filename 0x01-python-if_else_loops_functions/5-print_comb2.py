78bc711f53d85e23cc9f22305342c1a3666ba45c
musangisilvia <musangisilvia@gmail.com>
2021-04-27 11:02:07 +0000

    remove one print statement

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    if i < 10:
-        print("0{:d}".format(i), end=", ")
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
     else:
-        print("{:d}".format(i), end=", ")
-        if i == 99:
-            print("{:d}".format(i))
+        print("{:d}".format(i))
