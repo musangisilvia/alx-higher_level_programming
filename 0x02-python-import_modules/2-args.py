d55c265ec09231c3aa4398f6be9385f3b5974c27
musangisilvia <musangisilvia@gmail.com>
2021-04-29 10:01:59 +0000

    add feature to stop code execution when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 
 import sys
 
-length = len(sys.argv)
+if __name__ == "__main___"
+    length = len(sys.argv)
 
-if length <= 1:
-    print("0 arguments")
-elif length == 2:
-    print("1 argument:")
-    print("1: {:s}".format(sys.argv[1]))
-else:
-    print("{:d} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+    if length <= 1:
+        print("0 arguments")
+    elif length == 2:
+        print("1 argument:")
+        print("1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
