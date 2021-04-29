a34fd6293a3719ec1d5ba3e018a20c0ed1375f72
musangisilvia <musangisilvia@gmail.com>
2021-04-29 19:02:13 +0000

    use exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    length = len(sys.argv)
+    length = len(argv)
     if length != 4:
         print("Usage: ./100-my_caluclator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     else:
-        operator = sys.argv[2]
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
+        operator = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
         if operator == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif operator == "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif operator == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif operator == "/" and b != 0:
+        elif operator == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+            exit(1)
