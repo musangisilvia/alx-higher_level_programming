d4b8dd9598c9e46ab45e96a8f452ad48fd32fd77
musangisilvia <musangisilvia@gmail.com>
2021-04-29 10:12:44 +0000

    add colon at the end of if

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,14 @@
 
 import sys
 
-if __name__ == "__main___"
+if __name__ == "__main__":
     length = len(sys.argv)
 
-    if length <= 1:
-        print("0 arguments.")
+    if length == 1:
+        print("{:d} arguments.".format(length - 1))
     elif length == 2:
-        print("1 argument:")
-        print("1: {:s}".format(sys.argv[1]))
+        print("{:d} argument:".format(length - 1))
     else:
         print("{:d} arguments:".format(length - 1))
-        for i in range(1, length):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+    for i in range(1, length):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
