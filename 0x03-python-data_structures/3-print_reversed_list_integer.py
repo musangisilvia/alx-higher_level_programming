cabd7f77e10c9c7e7a401336e730d1b957df0bcd
musangisilvia <musangisilvia@gmail.com>
2021-05-05 18:40:34 +0000

    check if list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
