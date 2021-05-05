169b4e29b00d671724bc960434b326f1b525aa3e
musangisilvia <musangisilvia@gmail.com>
2021-05-05 19:02:05 +0000

    remove the complicated addition statement

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,19 +4,17 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     lenA = len(tuple_a)
     lenB = len(tuple_b)
-    if lenA >= 2 and lenB >= 2:
-        res = tuple(map(lambda i, j: i + j, tuple_a[0:2], tuple_b[0:2]))
-        return res
-    elif lenB < 2 and lenB != 0:
-       lsb = list(tuple_b)
-       lsb.append(0)
-       tb = tuple(lsb)
-       res1 = tuple(map(lambda i, j: i + j, tuple_a[0:2], tb[0:2]))
-       return res1
-    elif lenB == 0:
-        ls = list(tuple_b)
-        ls.append(0)
-        ls.append(0)
-        tp = tuple(ls)
-        res = tuple(map(lambda i, j: i + j, tuple_a[0:2], tp[0:2]))
-        return res
+# check for tuple_a
+    if lenA < 1:
+        tuple_a = (0, 0)
+    elif lenA < 2:
+        tuple_a = (tuple_a[0], 0)
+
+# check for tuple_b
+    if lenB < 1:
+        tuple_b = (0, 0)
+    elif lenB < 2:
+        tuple_b = (tuple_b[0], 0)
+
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return res
