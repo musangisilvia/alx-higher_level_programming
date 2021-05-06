aab30aa9902275a40fc6966790ca8eeeb8401a01
musangisilvia <musangisilvia@gmail.com>
2021-05-06 12:21:05 +0000

    remove the import

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 
-from copy import deepcopy
-
-
 # square_matrix_simple - computes the square value of all ints if a matrix.
 def square_matrix_simple(matrix=[]):
-    new_matrix = deepcopy(matrix)
+    new_matrix = [[0 for _ in range(len(matrix[0]))]
+    for _ in range(len(matrix))]
     for i in range(len(new_matrix)):
         for j in range(len(new_matrix[i])):
-            new_matrix[i][j] *= new_matrix[i][j]
+            new_matrix[i][j] = matrix[i][j]*matrix[i][j]
     return new_matrix
