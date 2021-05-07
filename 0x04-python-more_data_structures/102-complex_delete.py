abb365790ce6d0442cd85fc306ef9e3c118d3b78
musangisilvia <musangisilvia@gmail.com>
2021-05-07 13:06:23 +0000

    add another for loop to actually delete more than one key

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,9 +3,13 @@
 
 # complex_delete - delets keys with a specific value in a dictionary.
 def complex_delete(a_dictionary, value):
+    keylist = []
     if value in a_dictionary.values():
-        for key in a_dictionary.keys():
-            if value == a_dictionary[key]:
-                del a_dictionary[key]
-                break
+        for key, val in a_dictionary.items():
+            if value == val:
+                keylist.append(key)
+
+        for kl in keylist:
+            if kl in a_dictionary:
+                del a_dictionary[kl]
     return a_dictionary
