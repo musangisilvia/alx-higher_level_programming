428b2aa6935b970a77fef55e2cd487162381ba48
musangisilvia <musangisilvia@gmail.com>
2021-05-18 12:49:16 +0000

    rename folder

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" 
+    Module contains: class Square
+"""
+class Square:
+    def __init__(self, size):
+        """
+            Initialization of instance attribute
+            Args:
+                size (int): size of the square.
+        """
+        self.__size = size
