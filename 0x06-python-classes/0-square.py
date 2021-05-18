e12368b746ae154a277f902aedbc3c65251257d2
musangisilvia <musangisilvia@gmail.com>
2021-05-18 13:09:40 +0000

    correct pep8 errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+""" Module contains an empty class Square."""
 
-""" 
-   Module contains an empty class Square. 
-"""
+
 class Square:
-    """
-        This is an empty class.
-    """
+    """ This is an empty class. """
     pass
+
