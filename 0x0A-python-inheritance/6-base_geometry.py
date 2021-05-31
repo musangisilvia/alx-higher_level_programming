e943615b5956272d711b4b6cff39041152cb39ca
musangisilvia <musangisilvia@gmail.com>
2021-05-31 15:17:21 +0000

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    5-base_geometry: class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry
+        Attributes: None.
+        Methods:
+            area() - raises an Exception
+    """
+    def area(self):
+        raise AttributeError("area() is not implemented")
