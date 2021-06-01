e9233fc26183c8476165cf32743c56a3a75d37bc
musangisilvia <musangisilvia@gmail.com>
2021-06-01 15:17:01 +0000

    add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,10 +10,14 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
-    arg_list = []
+
     filename = "add_item.json"
-    for arg in sys.argv:
+
+    try:
+        arg_list = load_from_json_file(filename)
+    except:
+        arg_list = []
+
+    for arg in sys.argv[1:]:
         arg_list.append(arg)
-    arg_list.pop(0)
     save_to_json_file(arg_list, filename)
-    load_from_json_file(filename)
