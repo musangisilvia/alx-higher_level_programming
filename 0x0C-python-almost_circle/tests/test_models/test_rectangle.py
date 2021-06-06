a1d087807811675252b5d7118f0ad2715fca6749
musangisilvia <musangisilvia@gmail.com>
2021-06-06 20:50:42 +0000

    add tests

--- 0x0C-python-almost_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_circle/tests/test_models/test_rectangle.py
@@ -5,27 +5,48 @@
 import unittest
 from models.rectangle import Rectangle
 
-r1 = Rectangle(10, 2)
-r2 = Rectangle(2, 10)
-r3 = Rectangle(10, 2, 1, 1, 12)
-
 
 class TestRectangle(unittest.TestCase):
-    def test_is_instance(self):
-        self.assertIsInstance(r1, Rectangle)
-        self.assertIsInstance(r2, Rectangle)
-        self.assertIsInstance(r3, Rectangle)
+    """
+        Testing rectangle
+    """
+    def setUp(self):
+        """
+            Initialize an insatnce of rectangle.
+            With width and height only.
+        """
+        self.r = Rectangle(20, 10)
 
-    def test_id(self):
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r3.id, 12)
+    def tearDown(self):
+        """
+            Deleting created instance
+        """
+        del self.r
 
-    def test_area(self):
-        self.assertEqual(r1.area(), 20)
-        self.assertEqual(r2.area(), 20)
-        self.assertEqual(r3.area(), 20)
+    def test_width(self):
+        """
+            Testing rectangle width getter
+        """
+        self.assertEqual(20, self.r.width)
 
-    def test_raises(self):
-        self.assertRaises(TypeError)
-        self.assertRaises(ValueError)
+    def test_height(self):
+        """
+            Testing the height getter
+        """
+        self.assertEqual(10, self.r.height)
+
+    def test_x(self):
+        """
+            Testing the x getter
+        """
+        self.r.x = 2
+        self.assertEqual(2, self.r.x)
+        self.assertEqual(0, self.r.y)
+
+    def test_y(self):
+        """
+            Testing the y getter
+        """
+        self.r.y = 2
+        self.assertEqual(2, self.r.y)
+        self.assertEqual(0, self.r.x)
