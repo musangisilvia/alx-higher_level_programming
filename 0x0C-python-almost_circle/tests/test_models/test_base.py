178d58e31a0439aaa696649dcf0a989542a747c3
musangisilvia <musangisilvia@gmail.com>
2021-06-07 08:41:44 +0000

    add tests

--- 0x0C-python-almost_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_circle/tests/test_models/test_base.py
@@ -5,17 +5,62 @@ import unittest
 from models.base import Base
 
 
-b1 = Base()
-b2 = Base()
-b3 = Base()
-b4 = Base(12)
-b5 = Base()
+class TestBaseClass(unittest.TestCase):
+    """
+        Test class for the base class.
+    """
+    def test_id_none(self):
+        """
+            initialise an instance of the base class with no id
+        """
+        b = Base()
+        self.assertEqual(1, b.id)
 
-
-class BaseClassTestCase(unittest.TestCase):
     def test_id(self):
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
-        self.assertEqual(b4.id, 12)
-        self.assertEqual(b5.id, 4)
+        """
+            Initialise an instance with id > 0
+        """
+        b = Base(12)
+        self.assertEqual(12, b.id)
+
+    def test_id_zero(self):
+        """
+            Initialise instance with id == 0
+        """
+        b = Base(0)
+        self.assertEqual(0, b.id)
+
+    def test_id_negative(self):
+        """
+            Initialise instance with id < 0
+        """
+        b = Base(-2)
+        self.assertEqual(-2, b.id)
+
+    def test_id_string(self):
+        """
+            Intialise instance with id is string
+        """
+        b = Base("Base")
+        self.assertEqual("Base", b.id)
+
+    def test_id_list(self):
+        """
+            Initialise instance with id is list
+        """
+        b = Base([1, 3, 6])
+        self.assertEqual([1, 3, 6], b.id)
+
+    def test_id_tuple(self):
+        """
+            Initialise instance with id is tuple
+        """
+        b = Base((1, 3))
+        self.assertEqual((1, 3), b.id)
+
+    def test_id_dict(self):
+        """
+            Initialise instance with id is dict
+        """
+        b = Base({'id': 12})
+        self.assertEqual({'id': 12}, b.id)
