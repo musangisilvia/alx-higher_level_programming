e75b4c746745f68f6107c73446d7b73e4f80a14f
musangisilvia <musangisilvia@gmail.com>
2021-06-08 13:02:15 +0000

    change the name of function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,10 +31,10 @@ class Rectangle(Base):
                 id (int): id of rectangle.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
