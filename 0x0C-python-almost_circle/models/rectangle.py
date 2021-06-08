dd13cc89fcf1289196630d6931c3ede97913f4b9
musangisilvia <musangisilvia@gmail.com>
2021-06-08 09:45:07 +0000

    remove validators in __init__

--- 0x0C-python-almost_circle/models/rectangle.py
+++ 0x0C-python-almost_circle/models/rectangle.py
@@ -30,24 +30,7 @@ class Rectangle(Base):
                 y (int)
                 id (int): id of rectangle.
         """
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if type(x) != int:
-            raise TypeError("x must be an integer")
-        if type(y) != int:
-            raise TypeError("y must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-
-        Base.__init__(self, id)
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
