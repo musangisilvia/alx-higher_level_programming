48a983458cce589e9bc6d719a65d280e65dc0b14
musangisilvia <musangisilvia@gmail.com>
2021-08-04 05:54:40 +0000

    remove if

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,8 +2,4 @@
 
 // Script that prints "JavaScript is amazing".
 
-if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  console.log('No arguments');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
