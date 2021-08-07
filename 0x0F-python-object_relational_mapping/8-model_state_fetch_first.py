d6dee5282f001407c0efe3a51036621644961c15
musangisilvia <musangisilvia@gmail.com>
2021-08-07 10:28:39 +0000

    add if to prevent execution

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,22 +10,21 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+if __name__ = '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                       sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
+    # create a session
+    session = Session()
 
-# create a session
-session = Session()
+    # extract first state
+    states = session.query(State).order_by(State.id).first()
 
-# extract first state
+    # print state
+    print("{}: {}".format(states.id, states.name))
 
-states = session.query(State).order_by(State.id).first()
-
-# print state
-print("{}: {}".format(states.id, states.name))
-
-session.close()
+    session.close()
