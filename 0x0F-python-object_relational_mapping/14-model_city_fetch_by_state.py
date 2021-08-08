ae96dec43633a89aa540d6aa516c68d8ad669394
musangisilvia <musangisilvia@gmail.com>
2021-08-08 06:41:48 +0000

    add if name is main

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,23 +12,24 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                       sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
+if __name__ = '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
-Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-# create a session
-session = Session()
+    # create a session
+    session = Session()
 
-# extract all cities in a state
-cities = session.query(State, City) \
-                .filter(State.id == City.state_id)
+    # extract all cities in a state
+    cities = session.query(State, City) \
+                    .filter(State.id == City.state_id)
 
-# print all states
+    # print all states
 
-for city in cities:
-    print("{}: ({}) {}".format(city.State.name, city.City.id, city.City.name))
+    for city in cities:
+        print("{}: ({}) {}".format(city.State.name, city.City.id, city.City.name))
 
-session.close()
+    session.close()
