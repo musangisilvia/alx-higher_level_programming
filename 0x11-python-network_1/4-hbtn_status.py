b3b3258b7e1c22e1965d31428ac7dfc74f3f2241
musangisilvia <musangisilvia@gmail.com>
2021-08-22 08:02:20 +0000

    add .text:

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ import requests
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r)))
-    print("\t- content: {}".format(r))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
