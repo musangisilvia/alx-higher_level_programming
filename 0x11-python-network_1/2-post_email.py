9f4a42561d0ece5bfd93b987704c63f4957c4112
musangisilvia <musangisilvia@gmail.com>
2021-08-22 07:22:55 +0000

    add .read()

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,8 +11,8 @@ import urllib.parse
 
 
 if __name__ == "__main__":
-    email = urllib.parse.urlencode({'email': sys.argv[2]})
-    email = email.encode('ascii')
+    email = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
     req = urllib.request.Request(sys.argv[1], email)
     with urllib.request.urlopen(req) as resp:
+        html = resp.read()
         print("{}".format(html.decode('utf-8')))
