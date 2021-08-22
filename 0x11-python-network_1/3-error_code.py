3e59c9d422aa779cabe4c5cdc00b68ac8971d3f8
musangisilvia <musangisilvia@gmail.com>
2021-08-22 07:47:53 +0000

    add decoding

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,5 +16,6 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(req) as resp:
             html = resp.read()
+            print("{}".format(html.decode('utf-8')))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
