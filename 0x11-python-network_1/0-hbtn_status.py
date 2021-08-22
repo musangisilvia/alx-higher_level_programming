ea8f8c76799ce4b387582480b6861a0c843f458e
musangisilvia <musangisilvia@gmail.com>
2021-08-22 06:33:32 +0000

    correct if stmt

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@
 import urllib.request
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.htbn.io/status') as resp:
         html = resp.read()
 
