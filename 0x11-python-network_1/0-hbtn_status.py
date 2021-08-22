711f1df1a1224f1339084997809500e5e56cc782
musangisilvia <musangisilvia@gmail.com>
2021-08-22 06:48:09 +0000

    decode utf8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
