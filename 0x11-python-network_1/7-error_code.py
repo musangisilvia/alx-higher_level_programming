a4c977e04d206ebcd504d8df3d2fe8485235e915
musangisilvia <musangisilvia@gmail.com>
2021-08-22 08:49:32 +0000

    change request method

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1])
+    r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
