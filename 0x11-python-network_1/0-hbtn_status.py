407a21a94f857e9896af231f3ee01319deb5e54e
musangisilvia <musangisilvia@gmail.com>
2021-08-22 06:39:11 +0000

    move print to within with

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,10 +8,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.htbn.io/status') as resp:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         html = resp.read()
 
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html))
