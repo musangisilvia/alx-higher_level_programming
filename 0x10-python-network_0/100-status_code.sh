5d26dbf4ebd66ba0523875b9c8ffa01bcc756d2f
musangisilvia <musangisilvia@gmail.com>
2021-08-23 18:46:11 +0000

    correct shellcheck errors

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # script that sends a request to a URL passed as an arg displays status cpde
-curl -s -w "%{http_code}" $1 -o /dev/null
+curl -s -w "%{http_code}" "$1" -o /dev/null
