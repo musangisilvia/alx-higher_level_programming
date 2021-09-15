7fb04c46d7db2212b954ffafa4aff010e4681625
musangisilvia <musangisilvia@gmail.com>
2021-09-15 09:46:54 +0000

    add task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-if (parseInt(movieId) < 7) {
+if (parseInt(movieId) < 8) {
   const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
   request(url, function (err, response, body) {
